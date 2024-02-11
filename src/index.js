44b9168848798a822d36b077fc6c6977315d2223
Vikash Rai <raivikash174@gmail.com>
2024-02-11 19:19:57 +0530

    complete the code of connecting db

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import connectDB from "./db/index.js";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+// connectDB();
+
+import express from "express";
+
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://raivikash174:vL5HSvYmfL7OiGbR@cluster0.ghllcob.mongodb.net/${DB_NAME}`
+    );
+    app.on("error", (error) => {
+      console.log("Not able to connect to db", error);
+      throw error;
+    });
+
+    app.listen(3000, () => {
+      console.log(`App is listening on port ${3000}`);
+    });
+  } catch (error) {
+    console.error("ERROR in connecting DB: ", error);
+    throw error;
+  }
+})();
